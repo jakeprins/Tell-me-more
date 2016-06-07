35fd3e8c9d6faaef46a2d29b343fa212a24855cd
jakeprins <jakeprins@outlook.com>
2016-06-07 13:41:25 +0200

    Adjusted tag model so tag show page can render poems, stories and authors

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
   belongs_to :taggable, polymorphic: true
   has_many :taggings
+
+  has_many :authors, through: :taggings, source: :taggable, source_type: 'Author'
+  has_many :poems, through: :taggings, source: :taggable, source_type: 'Poem'
+  has_many :short_stories, through: :taggings, source: :taggable, source_type: 'ShortStory'
 end
